8a052e697a56badcb80ca131fcb01751f985e1de
Steve Cao <steve.cao7@gmail.com>
2018-10-26 13:05:51 -0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import Image from '../components/image'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>
+      Hi, my name is Steve I am a scientist turned software engineer. I love
+      learning and building new things! Look forward to building with everyone.
+      On a personal note, I like fried chicken, basketball, board games, and
+      video games.
+    </p>
+    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage
